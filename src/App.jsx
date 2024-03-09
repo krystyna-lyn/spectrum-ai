5a3e1ee4085f44625300b23a895894e882162a75
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-03-09 20:33:52 +0000

    add reusable Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Button from "./components/Button.jsx";
+import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div className="pt-[4.5rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-20" href="#login">
+          Login
+        </Button>
+        <ButtonGradient />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
