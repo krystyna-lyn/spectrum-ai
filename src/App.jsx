c9341ebe6a0116d978eae973866753892b0c10a1
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-03-10 21:44:17 +0000

    add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Button from "./components/Button.jsx";
 import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import Header from ".//components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <div className="pt-[4.5rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-20" href="#login">
-          Login
-        </Button>
+       <Header/>
         <ButtonGradient />
       </div>
     </>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,19 @@
-import React from "react";
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
-const Button = ({ className, href, onClick, children, px, white }) => {
-  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1
-  ${px || "px-7"} ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+const object = {
+  className: "black",
+  children:  "Click me!",
+  white: "red"
+}
 
-  const spanClasses = "relativ z-10";
+const Button = ({ className, href, onClick, children, px, white }) => {
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+  const spanClasses = "relative z-10";
+
   const renderButton = () => (
-    <button className={classes}>
+    <button className={classes} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
