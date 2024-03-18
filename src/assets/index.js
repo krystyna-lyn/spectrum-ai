3021afc3e8865e9ddbe01efd16c5a2c881be408f
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-03-18 20:38:21 +0000

    add icons, add Hero component

---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+import plusSquare from "./plus-square.svg";
+import file02 from "./file-02.svg";
+import searchMd from "./search-md.svg";
+import background from "./background.jpg";
+import curve from "./hero/curve.png";
+import homeSmile from "./home-smile.svg";
+import heroBackground from "./hero/hero-background.jpg";
+import robot from "./hero/robot.jpg";
+
+
+export {
+    plusSquare,
+    file02,
+    searchMd,
+    background,
+    curve,
+    homeSmile,
+    heroBackground,
+    robot
+};
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { curve, heroBackground, robot } from "../assets";
+import Button from "./Button";
+import Section from "./Section";
+import { useRef } from "react";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const parallaxRef = useRef(null);
 
-export default Hero
+  return (
+    <Section
+      className="pt-[12rem] -mt-[5.25rem]"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+      <div className="container relative" ref={parallaxRef}>
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
+          <h1 className="h1 mb-6">
+            Unlock the potential of AI interaction with
+            <span className="inline-block relative">
+              Spectrum AI{" "}
+              <img
+                src={curve}
+                className="absolute top-full left-0 w-full xl:-mt-2"
+                width={624}
+                height={28}
+                alt="Curve"
+              />
+            </span>
+          </h1>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+            Tap into the capabilities of AI with Spectrum, the innovative AI chat application. Boost your efficiency and productivity with Spectrum AI, your gateway to enhanced communication and collaboration.
+          </p>
+          <Button href="/pricing" white>
+            Get started
+          </Button>
+
+
+        </div>
+
+      </div>
+
+
+    </Section>
+  );
+};
+
+export default Hero;
