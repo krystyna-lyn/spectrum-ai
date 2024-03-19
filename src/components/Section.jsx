f8cc7d0124827e41e3997cb14ee5f4097a7464c2
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-03-19 21:05:14 +0000

    Update Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -12,8 +12,12 @@ const Section = ({
   return (
     <div
       id={id}
-      className={`relative ${padding || "py-10 lg:py-16 xl:p-20"}
-    ${crosses ? "lg:py-40 xl:py-60" : ""} ${className || ""}`}
+      className={`
+      relative 
+      ${padding ||
+        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+        } 
+      ${className || ""}`}
     >
       {children}
 
@@ -24,11 +28,10 @@ const Section = ({
         {crosses && (
           <>
             <div
-              className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1
-    ${crossesOffset && crossesOffset} lg:block lg:right-7.5 xl:right-10`}
-            >
-              <SectionSvg />
-            </div>
+              className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 ${crossesOffset && crossesOffset
+                } pointer-events-none lg:block xl:left-10 right-10`}
+            />
+            <SectionSvg crossesOffset={crossesOffset} />
           </>
         )}
       </div>
