1964e1f1b5cb3a00cdc1f8e2f2c0c2a0b8c419a7
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-03-26 12:05:48 +0000

    add Heading

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -8,7 +8,7 @@ const Benefits = () => {
             <div className="container relative z-2">
                 <Heading
                     className="md:max-w-md lg:max-w-2xl"
-                    title="Chat Smarter, Not Harder with Brainwave"
+                    title="Chat Smarter with Spectrum AI"
                 />
 
                 <div className="flex flex-wrap gap-10 mb-10">
