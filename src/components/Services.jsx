135a9ecacb67f817177358593acb81732b062a83
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-04-08 11:50:51 +0200

    update Services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,6 +4,7 @@ import Heading from './Heading'
 import Generating from './Generating'
 import { service1, service2, service3, check } from "../assets";
 import { services, servicesIcons } from '../constants';
+import { PhotoChatMessage } from './design/Services'
 
 const Services = () => {
     return (
@@ -58,6 +59,16 @@ const Services = () => {
                                     alt="robot"
                                 />
                             </div>
+
+                            <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+                                <h4 className="h4 mb-4">Photo editing</h4>
+                                <p className="body-2 mb-[3rem] text-n-3">
+                                    Automatically enhance your photos using our AI app&apos;s
+                                    photo editing feature. Try it now!
+                                </p>
+                            </div>
+
+                            <PhotoChatMessage />
 
                         </div>
 
